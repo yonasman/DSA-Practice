a16f6529e74ca1425e0e4d810f629ef43f7756d5
Yonas Negese M <yonasnegese250@gmail.com>
2024-07-15 10:17:41 +0300

    Practice some dsa

--- first.py
+++ first.py
@@ -1,6 +1,58 @@
+# practice python
+# exercise 1
+# *************************************
 def date_slicer(date):
     year = date[0:4]
     month = date[5:7]
     day = date[8]
     return f"Year: {year} Month: {month} day:{day}"
-print(date_slicer("2020-23-4"))
+# print(date_slicer("2020-23-4"))
+
+# exercise 2
+# **************************************
+def reverse_string(str):
+    str = str[-1::-1]
+    return str
+# print(reverse_string("hey"))
+
+# exercise 3
+# **************************************
+def generate_right_triangle():
+    for i in range(1,6):
+        for j in range(0,1):
+            print("* " * i)
+# generate_right_triangle()
+
+# exercise 4
+# *************************************
+# def GCF(num1,num2):
+#     if(num1 > num2): 
+#         n = num1
+#     else: 
+#         n = num2
+#     max = 0
+#     for i in range(1,n):
+#         if(num1 % i == 0 and num2 % i == 0):
+#             max = i
+#     return max
+# print(GCF(8,4))
+# way 2
+def GCF(num1, num2):
+    while num2:
+        num1, num2 = num2, num1 % num2
+    return num1
+# print(GCF(4,8))
+
+# exercise 5
+# *****************************************
+def smallerNumbersThanCurrent(arr):
+  output = []
+  
+  for n in arr:
+    count = 0
+    for m in arr:
+      if(n > m):
+         count += 1
+    output.append(count)
+  return output
+print(smallerNumbersThanCurrent([7,7,7,7]))
