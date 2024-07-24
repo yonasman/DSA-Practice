4e212e960f4897b5d9235af8d35801b2fd0b27f0
Yonas Negese M <yonasnegese250@gmail.com>
2024-07-24 11:09:35 +0300

    Add more dsa practice

--- first.py
+++ first.py
@@ -381,9 +381,40 @@ The indices where nums[i] == 2 are 1 and 2.
 # return a list of index
 def find_target_index(nums,target):
     n = len(nums)
+    nums.sort()
     new_arr = []
     for i in range(n):
         if(nums[i] == target):
             new_arr.append(i)
     return new_arr
-print(find_target_index([1,2,5,2,3],5))
+# print(find_target_index([1,2,5,2,3],5))
+"""
+Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+
+Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+
+ 
+
+Example 1:
+
+Input: g = [1,2,3], s = [1,1]
+Output: 1
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+You need to output 1.
+
+"""
+# pseudo code
+# define a func named findContentChildren that takes two list params
+# 
+def findContentChildren(g,s):
+    g.sort()
+    s.sort()
+    content,i,j = 0,0,0
+    while i < len(g) and j < len(s):
+        if s[j] >= g[i]:
+            content += 1
+            i += 1
+        j += 1
+    return content
+# print(findContentChildren([1,2,3],[]))
