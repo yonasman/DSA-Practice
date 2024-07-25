a3ba332aa7737b344b6da3240f4b01fe87a6bf74
Yonas Negese M <yonasnegese250@gmail.com>
2024-07-25 19:31:31 +0300

    Practice sortColors dsa

--- first.py
+++ first.py
@@ -648,3 +648,37 @@ def isStrictlyPalindromic(n):
                 p2 -= 1
     return True
 # print(isStrictlyPalindromic(9))
+"""
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+"""
+
+def sortColors1(nums):
+    for i in range(len(nums)):
+        for j in range(i + 1,len(nums)):
+            if(nums[i] > nums[j]):
+                nums[i],nums[j] = nums[j],nums[i]
+    return nums
+# print(sortColors1( [2,0,1]))
+
+# two pointer
+def sortColors(nums):
+    low, mid, high = 0, 0, len(nums) - 1
+    
+    while(mid <= high):
+        if nums[mid] == 0:
+            nums[low],nums[mid] = nums[mid],nums[low]
+            mid += 1
+            low += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid],nums[high] = nums[high],nums[mid]
+            high -= 1
+    return nums
+
+# print(sortColors([2,0,2,1,1,0]))
+    
