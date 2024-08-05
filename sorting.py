ffbc2f9f9e01a82eca13eea8242922c2694d3e86
Yonas Negese M <yonasnegese250@gmail.com>
2024-08-05 19:43:37 +0300

    Practice two pointers

--- sorting.py
+++ sorting.py
@@ -452,3 +452,53 @@ def pivotIndex( nums):
             p1 +=1
         return -1
 # print(pivotIndex([1,2,3]))
+# *********************************
+# TWO POINTERS PRACTICE
+# *********************************
+# brute force for merging array
+def merger(nums1,nums2):
+    newArr = []
+    for i in range(len(nums1)):
+        newArr.append(nums1[i])
+    for j in range(len(nums2)):
+        newArr.append(nums2[j])
+    newArr.sort()
+    return newArr
+# print(merger([1,2,3,4],[2,2,3]))
+def pointerMerger(nums1,nums2):
+    mergedArray = []
+    p1,p2 = 0,0 
+    while(p1 < len(nums1) and p2 < len(nums2)):
+        if nums1[p1] <= nums2[p2]:
+            mergedArray.append(nums1[p1])
+            p1 += 1
+        else:
+            mergedArray.append(nums2[p2])
+            p2 += 1
+    # handling the remaining elements
+    while(p1 < len(nums1)):
+        mergedArray.append(nums1[p1])
+        p1 += 1
+    while(p2 < len(nums2)):
+        mergedArray.append(nums2[p2])
+        p2 += 1
+    return mergedArray
+# print(pointerMerger([1,2,3,4],[2,2,3]))
+def mergeArr(nums1,m,nums2,n):
+    p1,p2 = m - 1,n - 1
+    p = m + n - 1
+    while(p1 >= 0 and p2 >= 0):
+        if(nums1[p1] > nums2[p2]):
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+    # handling the remaining elements
+    while(p2 >= 0):
+        nums1[p] = nums2[p2]
+        p2 -= 1
+        p -= 1
+    return nums1
+print(mergeArr(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3))
