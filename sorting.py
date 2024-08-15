ccf40803dcd249b6990767a0d53f0252094cbca7
Yonas Negese M <yonasnegese250@gmail.com>
2024-08-15 08:59:52 +0300

    Practice Dsa

--- sorting.py
+++ sorting.py
@@ -757,4 +757,27 @@ def finalValueAfterOperations(operations):
         else:
             x += 1
     return x
-print(finalValueAfterOperations(["X++","++X","--X","X--"]))
+# print(finalValueAfterOperations(["X++","++X","--X","X--"]))
+# Finding Maximum Sum Sub array of Size k
+def maxSumSubArray(nums,k):
+    n = len(nums)
+    if n < k:
+        return -1
+    
+    current_sum = sum(nums[:k])
+    max_sum = current_sum
+    for i in range(k,n):
+        current_sum += nums[i] - nums[i - k]
+        max_sum = max(max_sum,current_sum)
+
+    return max_sum
+# print(maxSumSubArray([1,2,3,4,5,6],7))
+def findMaxAverage(nums,k):
+    n = len(nums)
+    current_sum = sum(nums[:k])
+    max_sum = current_sum
+    for i in range(1,n - k + 1):
+        current_sum = sum(nums[i:k + i])
+        max_sum = max(current_sum,max_sum)
+    return max_sum / k
+print(findMaxAverage(nums = [5], k = 1))
