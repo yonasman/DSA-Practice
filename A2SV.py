72f03bc6c04ae7904cd5076f14c5c9e0523f4386
Yonas Negese M <yonasnegese250@gmail.com>
2024-08-17 17:43:44 +0300

    Practice dsa

--- A2SV.py
+++ A2SV.py
@@ -50,3 +50,22 @@ def mergeAlternately(word1,word2):
             mergedString += word1[n2:]
     return mergedString
 # print(mergeAlternately(word1 = "abc", word2 = "pqrzx"))
+def merge(nums1, m, nums2,n):
+    p = m + n - 1
+    p1 = m - 1
+    p2 = n - 1
+    
+    while p1 >= 0 and p2 >= 0:
+        if nums2[p2] > nums1[p1]:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        else:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        p -= 1
+    while p2 >= 0:
+        nums1[p] = nums2[p2]
+        p2 -= 1
+        p -= 1
+    print(nums1)
+merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [4,5,6], n = 3)
