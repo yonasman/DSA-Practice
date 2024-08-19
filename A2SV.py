8c6678131bf57a36f5c4e029b7fd339ad0c8c4c3
Yonas Negese M <yonasnegese250@gmail.com>
2024-08-19 16:40:10 +0300

    Practice dsa

--- A2SV.py
+++ A2SV.py
@@ -152,3 +152,31 @@ def majorityElement(nums):
             d[num] = 1
     return max(d,key = d.get)
 # print(majorityElement([3,3,4]))
+"""
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x.
+Record a new score of x.
+'+'.
+Record a new score that is the sum of the previous two scores.
+'D'.
+Record a new score that is the double of the previous score.
+'C'.
+Invalidate the previous score, removing it from the record.
+Return the sum of all the scores on the record after applying all the operations.
+"""
+def calPoints(ope):
+    result = []
+    for op in ope:
+        if op == "C":
+            result.pop()
+        elif op == "D":
+            result.append(result[-1] * 2)
+        elif op == "+":
+            result.append(result[-1] + result[-2])
+        else:
+            result.append(int(op))
+    return sum(result)
+# print(calPoints(["5","2","C","D","+"]))
