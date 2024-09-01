d5cb9ee9c225765d8eaa02a70d538ab0221e3ee7
Yonas Negese M <yonasnegese250@gmail.com>
2024-09-01 09:36:45 +0300

    Practice prefix sum

--- PrefixSum.py
+++ PrefixSum.py
@@ -105,3 +105,63 @@ def largestAltitude2(gain):
         max_altitude = max(max_altitude, current_altitude)
     return max_altitude
 # print(largestAltitude2([-5,1,5,0,-7]))
+def findMiddleIndex(nums):
+    n = len(nums)
+    leftSum = 0
+    totalSum = sum(nums)
+    
+    for i in range(n):
+        rightSum = totalSum - leftSum - nums[i]
+        if leftSum == rightSum:
+            return i
+        leftSum += nums[i]
+    return -1
+# print(findMiddleIndex([2,3,-1,8,4]))
+def sum_of_all_subarrays(nums):
+    n = len(nums)
+    totalSum = 0
+    
+    for i in range(n):
+        subArraySum = 0
+        for j in range(i,n):
+            subArraySum += nums[j]
+            totalSum += subArraySum
+    return totalSum
+# print(sum_of_all_subarrays([1,2,3]))
+def sum_of_all_subarrays2(nums):
+    n = len(nums)
+    totalSum = 0
+    
+    for i in range(n):
+        totalSum += nums[i] * (i + 1) * (n - i)
+    return totalSum
+# print(sum_of_all_subarrays2([1,2,3]))
+def EquilibriumIndex(nums):
+    n = len(nums)
+    leftSum = [0] * n
+    leftSum[0] = nums[0]
+    rightSum = [0] * n
+    rightSum[n - 1] = nums[n - 1]
+    
+    for i in range(1,n):
+        leftSum[i] = leftSum[i - 1] + nums[i]
+        
+    for j in range(n - 2, -1, -1):
+        rightSum[j] = rightSum[j + 1] + nums[j]
+    for i in range(n):
+        for j in range(n):
+            if i == j and leftSum[i] == rightSum[j]:
+                return i
+# print(EquilibriumIndex([1, 7, 3, 6, 5, 6]))
+def EquilibriumIndex2(nums):
+    totalSum = sum(nums)
+    leftSum = 0
+    n = len(nums)
+    
+    for i in range(n):
+        rightSum = totalSum - leftSum - nums[i]
+        if rightSum == leftSum:
+            return i
+        leftSum += nums[i]
+    return -1
+# print(EquilibriumIndex2([1, 7, 3, 6, 5, 6]))
